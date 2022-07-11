0a4265d9bc18fda167f103ebfc67b4af5cb21070
anderson.silva.agi <RtEJD2ATyzxuuXFdS7KD>
2022-07-11 17:30:47 -0300

    finished

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -63,9 +63,9 @@ SOME5=Cunhou NFT #1: Que legal isso!!!
 SOME6=Cunhou NFT #2: Estou muito feliz!!!
 SOME7=Cunhou NFT #3: Por isso!!!
 
-SOME0=MyEpicNFT 3.0
+SOME0=MyEpicNFT 4.0
 SOME2=Deploying contracts with account:  0x7e4d9Fc4bdec1fe100F60041C372270F0E2eDD97
-SOME3=Account balance:  68838689036186359
-SOME4=WavePortal address:  0x65334eea0841B24e9a96bAFa58A7943407074B4B
+SOME3=Account balance:  20609032326788783
+SOME4=WavePortal address:  0x7c61f083d7C7CdC5D45CcC36be637b064A991E53
 SOME5=Cunhou NFT #1: Que legal isso!!!
 */
